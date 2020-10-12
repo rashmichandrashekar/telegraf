33e44ad954376907719a0f3e972fcd65069f2d6d
Rashmi Mysore Chandrashekar <rashmy@microsoft.com>
2020-10-12 15:41:43 -0700

    changing to secure port

--- plugins/inputs/prometheus/kubernetes.go
+++ plugins/inputs/prometheus/kubernetes.go
@@ -128,7 +128,7 @@ func (p *Prometheus) watch(ctx context.Context, client *k8s.Client) error {
 				return err
 			}
 			nodeIP := os.Getenv("NODE_IP")
-			podsUrl := fmt.Sprintf("https://%s:10255/pods", nodeIP)
+			podsUrl := fmt.Sprintf("https://%s:10250/pods", nodeIP)
 			req, err := http.NewRequest("GET", podsUrl, nil)
 			req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", string(bearerToken)))
 			resp, err := client.Do(req)
